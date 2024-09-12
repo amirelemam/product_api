69a3b6436f0064bf4289ddb7e85b30c99732384f
Amir Elemam <amirelemam@gmail.com>
2024-09-12 01:11:30 -0300

    docs: add swagger docs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('Health Check')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('health')
+  @ApiOperation({ summary: 'Health check' })
+  @ApiResponse({
+    status: 200,
+    description: 'Returns if alive',
+    schema: {
+      type: 'string',
+      example: "I'm alive!",
+    },
+  })
   health() {
     return this.appService.getHealth();
   }
